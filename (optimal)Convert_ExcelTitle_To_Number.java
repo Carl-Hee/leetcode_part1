e131fa0c9dc1ba1fc856dbb30dbdc81afe72a067
Carl-Hee <hunanmeitan@gmail.com>
2016-03-24 23:19:10 +0800

    Update (optimal)Convert_ExcelTitle_To_Number.java

--- (optimal)Convert_ExcelTitle_To_Number.java
+++ (optimal)Convert_ExcelTitle_To_Number.java
@@ -1,1 +1,12 @@
 public class ExcelTitleToNum {
+  
+  public int covertTonum(String s) {
+    int result = 0;
+    for (int i = 0; i < s.length(); i++) {
+        result *= 26;
+        result += (s.charAt(i) - 'A') + 1;
+      }
+    return result;
+  }
+  
+}
